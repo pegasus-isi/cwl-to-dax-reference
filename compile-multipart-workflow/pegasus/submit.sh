3056ebf11f5486c0a3d7441064447df2b2762d81
Ryan Tanaka <tanaka@isi.edu>
2019-10-25 16:49:53 -0700

    PM-1399 added run scripts

---
+++ compile-multipart-workflow/pegasus/submit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+export TOP_DIR=`pwd`
+
+export WORK_DIR=$HOME/workflows
+if ls /local-scratch/ >/dev/null 2>&1; then
+    export WORK_DIR=/local-scratch/$USER/workflows
+fi
+mkdir -p $WORK_DIR
+
+export RUN_ID=test-workflow-`date +'%s'`
+
+export INPUT_DIR=$(pwd)/../
+
+#envsubst < tc.txt.template > tc.txt
+envsubst < sites.xml.template > sites.xml
+envsubst < ../input.yml.template > ../input.yml
+
+# generate workflow
+../../pegasus-cwl-converter.py --debug ../workflow.cwl ../input.yml ./workflow.xml
+
+# plan and submit the  workflow
+pegasus-plan \
+    --conf pegasus.conf \
+    --force \
+    --dir $WORK_DIR \
+    --relative-dir $RUN_ID \
+    --sites condorpool \
+    --output-site local \
+    --dax workflow.xml \
+    --cluster horizontal \
+    --submit
